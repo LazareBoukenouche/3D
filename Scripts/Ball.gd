64e9a5a6993fc331e5f11eb44eb9914925a58bb4
LazareBoukenouche <lazare.boukenouche@gmail.com>
2023-09-11 19:56:02 +0200

    Handled the camera and player movements and rotations

--- Scripts/Ball.gd
+++ Scripts/Ball.gd
@@ -1,47 +1,57 @@
 extends CharacterBody3D
 
+signal can_interact
+
+signal can_not_interact
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 var hold_object: bool = false
 
-signal can_interact
+var mouse_sensitivity := 0.010
+var horizontal_pivot_input := 0.0
+var vertical_pivot_input := 0.0
 
-signal can_not_interact
+@onready var player = get_node(".")
+@onready var horizontal_pivot = $Horizontal_pivot
+@onready var vertical_pivot = $Horizontal_pivot/Vertical_pivot
+@onready var camera = $Horizontal_pivot/Vertical_pivot/Camera3D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+func _ready() -> void:
+	
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)	
+	
 func _physics_process(delta):
-	# Add the gravity.
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	move_and_slide()
-	
-func _process(_delta):
+		
+	var input := Vector3.ZERO
+	input.x = Input.get_axis("move_left", "move_right")
+	input.z = Input.get_axis("move_up", "move_down")
 	
 	
+	horizontal_pivot.rotate_y(horizontal_pivot_input)
+	vertical_pivot.rotate_x(vertical_pivot_input)
+	vertical_pivot.rotation.x = clamp(vertical_pivot.rotation.x, 
+		deg_to_rad(-30), 
+		deg_to_rad(30)
+	)
+	
+	horizontal_pivot_input = 0.0
+	vertical_pivot_input = 0.0
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
 	if Input.is_action_just_pressed("attack"):
-		print("attack")
+		print(input)
+		pass
 		
 	if Input.is_action_just_pressed("take"):
 		# Check if object in area has the property : interactable
@@ -62,37 +72,46 @@ func _process(_delta):
 					if elem.position.distance_to(self.position) < shortest_distance or shortest_distance == -1:
 						selected_node = elem
 				selected_node.reparent(self)
-					
-					
-					
-			
-	
+		
 	if move_and_slide():
 		for i in get_slide_collision_count():
-			
 			var collision = get_slide_collision(i)
-			
 			if collision.get_collider().name == "Sword":
 				pass
-			
-	
-	
-	
+				
+	if not is_on_floor():
+		velocity.y -= gravity * delta
 
-func _input(event):
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = (horizontal_pivot.basis * input).normalized()
+#
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+	
+func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		if event is InputEventMouseMotion:
-#			get_node("Camera_pivot").rotation
-			
-			
-			if event.relative[0] < -2:
-				get_node("Camera_pivot").rotation.y += 0.1
-			if event.relative[0] > 2:
-				get_node("Camera_pivot").rotation.y -= 0.1
-			if event.relative[1] > 2:
-				get_node("Camera_pivot").rotation.x -= 0.1
-			if event.relative[1] < -2:
-				get_node("Camera_pivot").rotation.x += 0.1
+			if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+				horizontal_pivot_input = - event.relative.x * mouse_sensitivity
+				vertical_pivot_input = - event.relative.y * mouse_sensitivity
+				
+	if !Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+#	
+				
 
 func _on_area_3d_body_entered(body):
 	
@@ -104,8 +123,6 @@ func _on_area_3d_body_entered(body):
 			get_node("Label3D").set_text(body.to_string()) 
 			get_node("Label3D").visible = true
 		
-
-
 func _on_area_3d_body_exited(body):
 	if "interactable" in body:
 		pass
@@ -116,3 +133,4 @@ func _on_area_3d_body_exited(body):
 	get_node("Label3D").set_text(body.to_string()) 
 	get_node("Label3D").visible = false
 	
+
--- Scripts/Level.gd
+++ Scripts/Level.gd
@@ -6,7 +6,7 @@ extends Node3D
 func _ready():
 	
 	
-	var camera = get_node("Ball/Camera_pivot/Camera3D")
+	
 	
 	
 	pass
@@ -16,12 +16,14 @@ func _ready():
 func _process(_delta):
 
 
-	if has_node("Ball"):
-		if Input.is_action_just_pressed("take"):
-			print(get_node("Ball").get_child((1)))
-			if get_node("Ball").get_child((1)):
-				get_node("Sword").queue_free()
+#	if has_node("Ball"):
+#		if Input.is_action_just_pressed("take"):
+#			print(get_node("Ball").get_child((1)))
+#			if get_node("Ball").get_child((1)):
+#				get_node("Sword").reparent($Ball)
+#				get_node("Sword").queue_free()
 
+	pass
 
 			
 
@@ -29,7 +31,7 @@ func _process(_delta):
 
 
 
-func _input(event):
+func _input(_event):
 #	if Input.is_mouse_button_pressed(1):
 #		if event is InputEventMouseMotion:
 #			print(event.relative)
